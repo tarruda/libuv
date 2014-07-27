7470a43f7f047c587c407fdfa77fe3ba3455a71b
Thiago de Arruda <tpadilha84@gmail.com>
2014-07-27 14:33:07 -0300

    fix x86-ubuntu

--- .ci/ubuntu-12.04-x86.sh
+++ .ci/ubuntu-12.04-x86.sh
@@ -1,7 +1,3 @@
-# Pins the version of the java package installed on the Travis VMs
-# and avoids a lengthy upgrade process for them.
-sudo apt-mark hold oracle-java7-installer oracle-java8-installer
-sudo apt-get update
 sudo apt-get install libc6-dev:i386 gcc-multilib
 export CFLAGS=-m32
 export LDFLAGS=-m32
