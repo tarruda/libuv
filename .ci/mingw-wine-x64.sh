20f94ea029cfc763f60e864c1dd005a7515a800d
Thiago de Arruda <tpadilha84@gmail.com>
2014-07-27 16:35:09 -0300

    try force mingw install

--- .ci/mingw-wine-x64.sh
+++ .ci/mingw-wine-x64.sh
@@ -1,4 +1,5 @@
 . "$CI_ROOT/mingw-patch.sh"
-sudo apt-get install wine mingw-w64
+sudo apt-get install --fix-missing mingw-w64
+sudo apt-get install --fix-missing wine
 export CI_CONFIGURE_OPTS=--host=x86_64-w64-mingw32
 . "$CI_ROOT/build-test.sh"
