045b4c41acc9ac5bde4158be535d2e58ae89c20d
Thiago de Arruda <tpadilha84@gmail.com>
2014-07-27 16:38:05 -0300

    install specific mingw packages

--- .ci/mingw-wine-x64.sh
+++ .ci/mingw-wine-x64.sh
@@ -1,5 +1,4 @@
 . "$CI_ROOT/mingw-patch.sh"
-sudo apt-get install --fix-missing mingw-w64
-sudo apt-get install --fix-missing wine
+sudo apt-get install binutils-mingw-w64-x86-64 gcc-mingw-w64-x86-64 wine
 export CI_CONFIGURE_OPTS=--host=x86_64-w64-mingw32
 . "$CI_ROOT/build-test.sh"
